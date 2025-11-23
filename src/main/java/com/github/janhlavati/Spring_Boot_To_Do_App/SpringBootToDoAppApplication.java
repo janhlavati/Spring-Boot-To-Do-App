2ea4f8c48a42912cbb8036b806e8d6039f3c7471
janhlavati <janhlavaty11@gmail.com>
2025-11-23 02:27:34 +0100

    Initial commit

---
+++ src/main/java/com/github/janhlavati/Spring_Boot_To_Do_App/SpringBootToDoAppApplication.java
@@ -0,0 +1,13 @@
+package com.github.janhlavati.Spring_Boot_To_Do_App;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootToDoAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootToDoAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/janhlavati/Spring_Boot_To_Do_App/SpringBootToDoAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.janhlavati.Spring_Boot_To_Do_App;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootToDoAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
